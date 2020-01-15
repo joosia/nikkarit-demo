438922c46faf34c836d8ea4e094a516e0953e064
L4150 <l4150@student.jamk.fi>
2020-01-15 15:26:41 +0200

    jotain höttöö

---
+++ src/components/Testi/Spinner/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Spinner.module.css';
+
+const Spinner = (props) => {
+    return (
+        <div className="Spinner"></div>
+    );
+}
+
+export default Spinner;
---
+++ src/containers/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Layout.module.css';
+import Testi from '../../components/Testi/Testi';
+
+const layout = (props) => {
+    return (
+        // React.Fragment on tässä tapauksessa Reactin vaatima ympäröivä komponentti, voisi olla myös esim div
+        <React.Fragment>
+            <Testi/>
+        </React.Fragment>
+    );
+}
+
+export default layout;
