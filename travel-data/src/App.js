072a28e4972802095a36ecd9052cc03ad6ef9697
L4150 <l4150@student.jamk.fi>
2020-01-15 09:41:03 +0200

    setupattu vähän

--- travel-data/src/App.js
+++ travel-data/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Layout from './containers/Layout/Layout';
 
 function App() {
   return (
-    
+    <Layout/>
   );
 }
 
---
+++ travel-data/src/containers/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Layout.module.css';
+
+const layout = (props) => {
+    return (
+        // React.Fragment on tässä tapauksessa Reactin vaatima ympäröivä komponentti, voisi olla myös esim div
+        <React.Fragment>
+            {/* Tähän rakennetaan leiska */}
+        </React.Fragment>
+    );
+}
+
+export default layout;
