760b36cd7db8c132be815ca7d341aae312824ee7
L4150 <l4150@student.jamk.fi>
2020-01-27 13:56:59 +0200

    juu

--- src/containers/Events/Events.js
+++ src/containers/Events/Events.js
@@ -2,28 +2,23 @@ import React from 'react';
 import EventCard from '../../components/EventCard/EventCard';
 import rp from 'request-promise';
 import $ from 'cheerio';
-import axios from 'axios';
 import classes from './Events.module.css';
 
 class Events extends React.Component {
 
     state = {
         url: "http://kalenteri.turku.fi/events/calendar",
-        events: [],
-        testEvent: {
-            name: '',
-            date: '',
-            imageUrl: '',
-            link: ''
-        }
+        events: []
     }
 
     componentDidMount() {
         this.ParseEvent('https://cors-anywhere.herokuapp.com/http://kalenteri.turku.fi/events/node/307620');
+        this.ParseEvent('https://cors-anywhere.herokuapp.com/http://kalenteri.turku.fi/events/node/303380');
+        this.ParseEvent('https://cors-anywhere.herokuapp.com/http://www.turku.fi/tapahtuma/pe-01032020-0900-ala-lyo-valokuvanayttely-naisiin-kohdistuva-vakivalta-aina-rikos')
     }
 
     GetEvents() {
-        axios.get('https://cors-anywhere.herokuapp.com/' + this.state.url)
+        rp('https://cors-anywhere.herokuapp.com/' + this.state.url)
         .then(response => {
             // console.log(response);
             // Hae urlit yms yms
@@ -34,19 +29,17 @@ class Events extends React.Component {
     ParseEvent = (url) => {
         return rp(url)
         .then(html => {
+
+            const newEvent = {
+                name: $('#l-content > div.event.node.node--event.view-mode-full.node--full.node--event--full > div.event__image--wrapper > div.event__image__content > div.event__image__content--bottom > div.event__title > h1', html).text(),
+                date: $('#l-content > div.event.node.node--event.view-mode-full.node--full.node--event--full > div.event__image--wrapper > div.event__image__content > div.event__image__content--bottom > div:nth-child(1)', html).text(),
+                imageUrl: $('#l-content > div.event.node.node--event.view-mode-full.node--full.node--event--full > div.event__image--wrapper > div.event__image > picture > source:nth-child(1)', html).attr('srcset'),
+                link: $('#l-content > div.event.node.node--event.view-mode-full.node--full.node--event--full > div.event__sidebar--top > div.event__website > a', html).attr('href')
+            };
+
             this.setState({
-                testEvent: {
-                    name: $('#l-content > div.event.node.node--event.view-mode-full.node--full.node--event--full > div.event__image--wrapper > div.event__image__content > div.event__image__content--bottom > div.event__title > h1', html).text(),
-                    date: $('#l-content > div.event.node.node--event.view-mode-full.node--full.node--event--full > div.event__image--wrapper > div.event__image__content > div.event__image__content--bottom > div:nth-child(1)', html).text(),
-                    imageUrl: $('#l-content > div.event.node.node--event.view-mode-full.node--full.node--event--full > div.event__image--wrapper > div.event__image > picture > source:nth-child(1)', html).attr('srcset'),
-                    link: $('#l-content > div.event.node.node--event.view-mode-full.node--full.node--event--full > div.event__sidebar--top > div.event__website > a', html).attr('href')
-                }
+                events: [...this.state.events, newEvent]
             })
-            // return {
-            //     eventName: $('#l-content > div.event.node.node--event.view-mode-full.node--full.node--event--full > div.event__image--wrapper > div.event__image__content > div.event__image__content--bottom > div.event__title > h1', html).text(),
-            //     eventDate: $('#l-content > div.event.node.node--event.view-mode-full.node--full.node--event--full > div.event__image--wrapper > div.event__image__content > div.event__image__content--bottom > div:nth-child(1)', html).text(),
-            //     eventBanner: $('#l-content > div.event.node.node--event.view-mode-full.node--full.node--event--full > div.event__image--wrapper > div.event__image > picture > source:nth-child(1)', html).attr('srcset')
-            // };
         })
         .catch(function(err) {
             console.log(err);
@@ -54,17 +47,24 @@ class Events extends React.Component {
     }
 
     render() {
+
+        const events = this.state.events.map(event => {
+            return (
+                <EventCard 
+                    url={event.link}
+                    date={event.date}
+                    name={event.name}
+                    imageUrl={event.imageUrl}
+                    rating={3.857}
+                />
+            );
+        })
+
         return (
             <div className={classes.Events}>
                 <h2>Tapahtumat paikassa Turku</h2>
                 <div>
-                    <EventCard 
-                        url={this.state.testEvent.link}
-                        date={this.state.testEvent.date}
-                        name={this.state.testEvent.name}
-                        imageUrl={this.state.testEvent.imageUrl}
-                        rating={3.857}
-                    />
+                    {events}
                     <EventCard 
                         url="http://www.google.fi" 
                         date="1.2.2020" 
